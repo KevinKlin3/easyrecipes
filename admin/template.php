fe4d9aea6339367ab375ee0741c92ff66536e2d6
KevinKlin3 <e1994727@student.dcccd.edu>
2022-03-29 18:52:32 -0400

    template change w/ bootstrap

--- admin/template.php
+++ admin/template.php
@@ -6,14 +6,19 @@ echo<<<HERE
         <title>Easy Recipes</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap 3 -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
         <!--FontAwesome kit-->
         <script src="https://kit.fontawesome.com/c0e800fc4a.js" crossorigin="anonymous"></script>
+        
         <!--External Style sheet-->
         <link rel="stylesheet"  type="text/css" href="admin\css\index.css">
     </head>
@@ -22,6 +27,7 @@ echo<<<HERE
             <img src="admin/images/hero2.jpg" alt="collage of food images">
         </header>
     <!-- Nav Bar -->
+    
 <ul>
   <li><a href="index.php">Home</a></li>
   <li><a href="gallery.php">Recipe Gallery</a></li>
