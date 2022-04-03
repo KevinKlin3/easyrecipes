73e93f102c4b8a89e252c7f2b05fed4e3a6e511d
KevinKlin3 <e1994727@student.dcccd.edu>
2022-04-03 09:58:56 -0400

    updates to template, added favicon, fixed header

--- admin/template.php
+++ admin/template.php
@@ -6,34 +6,34 @@ echo<<<HERE
         <title>Easy Recipes</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--Bootstrap4-->
+        <!--bootstrap5-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        
+    <!-- jQuery library -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
 
-    <!--Jquery-->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-
-    <!--jsPopper-->
+    <!--javaScript-->
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-    <!--JavaScript-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!--FontAwesome kit-->
         <script src="https://kit.fontawesome.com/c0e800fc4a.js" crossorigin="anonymous"></script>
         
     <!--External Style sheet-->
         <link rel="stylesheet"  type="text/css" href="admin\css\index.css">
+    <!-- favicon -->
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon.ico">
     </head>
     <body>
-        <header>
-            <img class="img-fluid" id="img-head" src="admin/images/hero2.jpg" alt="collage of food images">
+        <header class="container-fluid">
+        <img class="img-fluid" src="admin\images\hero3.jpg" alt="collage of food images">
         </header>
         <!-- Nav Bar -->
-            <nav class="navbar navbar-expand-sm bg-light navbar-dark">
+            <nav class="navbar navbar-expand-sm  navbar sticky-top">
                 <ul class="navbar-nav">
-                    <li>
-                        <img src="admin/images/logo.png" class="img-thumbnail" alt="logo for page">
+                    <li "container-fluid">
+                        <img  src="admin/images/logo.png" class="navbar-brand" id="logo" alt="logo for page">
                     </li>
-                    <li "nav-item active">
+                    <li class="navbar-nav">
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="navbar-nav">
@@ -45,13 +45,11 @@ echo<<<HERE
                     <li class="navbar-nav">
                         <a class="nav-link" href="#">Contact us</a>
                     </li>
-                    <li>
-                        <form class="form-inline" action="">
-                            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                                <button class="btn btn-success" id="btnSearch" type="submit">Search</button>
-                        </form>
-                    </li>
                 </ul>
+                <form class="d-flex right" id="searchbar">
+                    <input class="form-control me-2" type="text" placeholder="Search">
+                        <button class="buttonSearch" type="button">Search</button>
+                </form>
             </nav>
         <!-- End of NavBar -->
 HERE;
